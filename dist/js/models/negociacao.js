80b5046c30a1ccf1c20b6d29881e4d7c4ad765a4
Felipe Tavares <felipe.tavares@outlook.com>
2024-01-23 06:11:02 -0300

    Finalizando modelo negociacao

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -5,7 +5,23 @@ export class Negociacao{
 
     constructor(data, quantidade, valor){
         this.#data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+
+    get data(){
+        return this.#data;
+    }
+
+    get quantidade(){
+        return this.#quantidade;
+    }
+
+    get valor(){
+        return this.#valor;
+    }
+
+    get volume(){
+        return this.#quantidade * this.#valor;
     }
 }
